9afe81929f85ea453d2e7a60abf9b86c4181577b
hoad-oan <dhoadoan@gmail.com>
2026-08-05 23:46:35 -0400

    Completed requirement 1-3
    Updated requirement 2 file name due to updated URL

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,76 @@
 import requests
 import pandas
+import os
+from bs4 import BeautifulSoup
 
+URL = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2024/"
+
+session = requests.Session() # enable the ability to reuse session connection and close at end of code
+
+def scrape_data():
+    try:
+        response = session.get(URL, timeout = 30)
+        html_data = response.text
+        print(response.status_code)
+        response.raise_for_status() # raise exception for response.status != 200
+        soup = BeautifulSoup(html_data, "html.parser")
+
+        tbody = soup.find("tbody")
+
+        if tbody:
+            for row in tbody.find_all("tr"):
+                row_data = []
+                for cell in row.find_all("td"):
+                    row_data.append(cell.get_text(strip=True))
+                    if len(row_data) > 1 and row_data[1] == "2025-01-24 05:23":
+                        print("Found file")
+                        target_file = row_data[0]
+                        return target_file
+
+        return None
+
+    except Exception as e:
+        print(f"Failed to get data: {e}")
+        return None
+    
+def download_target_file(target_url: str, saved_file_path: str):
+    response = session.get(target_url, stream=True, timeout = 30)
+    response.raise_for_status()
+
+    with open(saved_file_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+# def open_file_and_find_record():
+
+# https://www.ncei.noaa.gov/data/local-climatological-data/access/2024/01001099999.csv
 
 def main():
-    # your code here
-    pass
+    target_file = scrape_data()
+    
+    if target_file is not None:
+        target_url = f"{URL}{target_file}"
+        # Get the directory where THIS script file lives
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Set the downloads folder path next to the script
+        download_dir = os.path.join(script_dir, "downloads")
+        os.makedirs(download_dir, exist_ok=True)
+        saved_file_path = os.path.join(download_dir, target_file)
+        download_target_file(target_url, saved_file_path)
+    else:
+        return None
+    # pass
 
 
 if __name__ == "__main__":
     main()
+
+session.close()
+
+
+# 1. Attempt to web scrap/pull down the contents of `https://www.ncei.noaa.gov/data/local-climatological-data/access/2024/`
+# 2. Analyze it's structure, determine how to find the corresponding file to `2024-01-19 10:27	` using Python.
+# 3. Build the `URL` required to download this file, and write the file locally.
+# 4. Open the file with `Pandas` and find the records with the highest `HourlyDryBulbTemperature`.
+# 5. Print this to stdout/command line/terminal.
